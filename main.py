55772bb0d87f17c1dde4acda15bf0340d4c27698
Nizzate <101407978+Nizzate@users.noreply.github.com>
2024-02-29 06:10:31 -0500

    Update main.py
    Finished

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import userChecker
+import time
 
-userChecker.checker(389997,5)
+
+count = input('How many accounts do you want to scan?\n')
+while count.isnumeric() == False:
+    print('Whatever you said was not a digit... Try again!\n')
+    count = input('How many accounts do you want to scan?\n')
+
+userID = input('What ID do you want to start from?\n')
+
+while userID.isnumeric() == False:
+    print('Whatever you said was not a digit... :( \n')
+    userID = input('What ID do you want to start from?\n')
+
+
+userChecker.checker(int(userID),int(count))
+
+print('\nAll done! :) This program was made by Nizzate on github!')
+print("Please check the same directory as this executable for a 'List.txt'!")
+time.sleep(5)
