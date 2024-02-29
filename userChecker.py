5cf70dbf627120e89f003840f5ca580e2002921b
Nizzate <101407978+Nizzate@users.noreply.github.com>
2024-02-29 06:10:53 -0500

    Update userChecker.py
    fixed some stuff

--- userChecker.py
+++ userChecker.py
@@ -1,7 +1,9 @@
 import requests
+import os
+import time
 def checker(userID,count):
     originalUserID = userID
-    while userID <= (originalUserID+count):
+    while userID <= (originalUserID+count)-1:
         url = "https://www.roblox.com/users/"+str(userID)+"/profile"
         getHTTPData(url)
         skimmedData = skimHTTPData(userID)
@@ -17,7 +19,7 @@ def getHTTPData(url):
     try:
         response = requests.get(url)
         responsePage = response.text
-        fileManager = open('output.txt','w')
+        fileManager = open('output.txt','w',encoding='utf8')
         fileManager.writelines(responsePage)
         fileManager.close()
         # If the request fails (404) then print the error.
@@ -45,11 +47,11 @@ def postHTTPData(url,json):
         404 Client Error: Not Found for url: 
         """
     except UnicodeEncodeError:
-        print('Some weird unicode encoding error happened. Your output file is fine though.')   
+        print('Some weird unicode encoding error happened. Your output file may or may not be affected. (Can affect data compilation!)')   
 
 
 def skimHTTPData(userID):
-    fileManager = open('output.txt','r')
+    fileManager = open('output.txt','r',encoding='utf8')
     
     accountsFound = []
     usernameInt = 0
@@ -58,28 +60,28 @@ def skimHTTPData(userID):
         if '@' in line:
             usernameInt += 1
             if usernameInt == 5:
-                print('found username')
+                print('Found username.')
                 #print(line)
                 foundData = 'username',line[line.find('@')+1:-1].replace('"\n','')
                 accountsFound.append(foundData)
 
         if '<div data-profileuserid=' in line:
-            print('found user id')
+            print('Found user ID.')
             foundData = 'userID',line[line.find('"')+1:line.find('"')*2].replace('"\n','')
             accountsFound.append(foundData)
 
         if 'data-friendscount=' in line:
-            print('found friend count')
+            print('Found friend count.')
             foundData = 'friends',line[line.find('"')+1:line.find('"')*2].replace('"\n','')
             accountsFound.append(foundData)
         
         if 'data-followerscount=' in line:
-            print('found follower count')
+            print('Found follower count.')
             foundData = 'followers',line[line.find('"')+1:line.find('"')*2].replace('"\n','')
             accountsFound.append(foundData)
         
         if 'data-followingscount=' in line:
-            print('found following count')
+            print('Found following count.')
             foundData = 'following',line[line.find('"')+1:line.find('"')*2].replace('"\n','')
             accountsFound.append(foundData)
     
@@ -87,25 +89,25 @@ def skimHTTPData(userID):
     url = 'https://users.roblox.com/v1/users/'+str(userID)
     getHTTPData(url)
     
-    fileManager = open('output.txt','r')
+    fileManager = open('output.txt','r',encoding='utf8')
     for line in fileManager:
         if '"created"' in line:
-            print('found join date')
+            print('Found join date.')
             foundData = 'created',line[line.find('"created":'):line.find('","is')].replace('"\n','').lstrip('"created":')
             accountsFound.append(foundData)
         if 'ned":true' in line:
-            print('BANNED USER FOUND')
-            foundData = 'isBanned',line[line.find('Banned":'):line.find(',"exter')].lstrip('Banned":').replace('"\n','')
-            accountsFound.append(foundData)
-            accountsFound.append(userID)
+            print('BANNED USER FOUND!!!')
+            accountsFound.append(line)
+            #accountsFound.append(userID)
         elif 'ned":false' in line:
-            print('not banned')
-            foundData = 'isBanned',line[line.find('Banned":'):line.find(',"exter')].lstrip('Banned":').replace('"\n','')
+            print('Not banned.')
+            foundData = 'isBanned',line[line.find('Banned":'):line.find(',"exter')].lstrip('Banned":').replace('"\n',''),'username',line[line.find('name":'):line.find(',"dis')].lstrip('name":').replace('"\n','')
             accountsFound.append(foundData)
         if '"hasVerifiedBadge"' in line:
-            print('found verification status (blue check mark)')
+            print('Found verification status. (Not email verificaion!!!)')
             foundData = 'hasVerifiedBadge',line[line.find('Badge":'):line.find(',"id":')].lstrip('Badge":').replace('"\n','')
             accountsFound.append(foundData)
+    #print(accountsFound)
     fileManager.close()
     
     url = 'https://presence.roblox.com/v1/presence/last-online'
@@ -115,16 +117,30 @@ def skimHTTPData(userID):
     fileManager = open('output.txt','r')
     for line in fileManager:
         if '"lastOnline"' in line:
-            print('found last seen date')
+            print('Found last seen date.')
             foundData = 'lastOnline',line[line.find('Online":'):line.find('"}]')].lstrip('Online":').replace('"\n','')
             accountsFound.append(foundData)
-
     return accountsFound
+'''
+    url = 'https://catalog.roblox.com/v1/users/'+str(userID)+'/bundles?limit=100&sortOrder=Asc'
+    getHTTPData(url)
+    
+    fileManager = open('output.txt','r',encoding='utf8')
+    for line in fileManager:
+        print(line)
+        if 'iBot' in line:
+            print('FOUND IBOT ACCOUNT!!!')
+            foundData = 'iBot','True'
+            accountsFound.append(foundData)
+    fileManager.close()'''
+
+    
 
 def writeData(skimmedData):
-    fileManager = open('data.txt','a')
+    fileManager = open('List.txt','a')
     fileManager.write(str(skimmedData).replace('[','').replace(']','\n'))
     fileManager.close()
+    os.remove('output.txt')
 
 
 
